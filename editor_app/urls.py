18af229a54207bc400dfe2318c5463ce551dbd78
Stanisław Góra <stasgora@gmail.com>
2020-06-05 21:10:03 +0200

    Users and authentication

--- editor_app/urls.py
+++ editor_app/urls.py
@@ -4,7 +4,10 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('auth/', views.auth, name='auth'),
+	path('logout/', views.logout, name='logout'),
 	path('home/', views.home, name='home'),
+
 	path('new/', views.new, name='new'),
 	path('edit/<int:formula_id>/', views.editor, name='editor'),
 	path('save/', views.save, name='save'),
--- editor_app/views.py
+++ editor_app/views.py
@@ -1,3 +1,7 @@
+import django
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404, redirect
 
 from editor_app.models import Formula
@@ -8,19 +12,27 @@ def index(request):
 
 
 def home(request):
+	if not request.user.is_authenticated:
+		return redirect('/')
 	return render(request, 'home.html')
 
 
 def editor(request, formula_id):
+	if not request.user.is_authenticated:
+		return redirect('/')
 	formula = get_object_or_404(Formula, id=formula_id)
 	return render(request, 'editor.html', {'formula': formula})
 
 
 def new(request):
+	if not request.user.is_authenticated:
+		return redirect('/')
 	return render(request, 'editor.html', {'formula': {'title': '', 'text': ''}})
 
 
 def save(request):
+	if not request.user.is_authenticated:
+		return redirect('/')
 	id = request.POST['id']
 	if id != '':
 		formula, _ = Formula.objects.get_or_create(id=id)
@@ -30,3 +42,27 @@ def save(request):
 	else:
 		Formula.objects.create(text=request.POST['text'], title=request.POST['title'])
 	return redirect('home')
+
+
+def auth(request):
+	if 'singIn' in request.POST:
+		if not User.objects.filter(username=request.POST['username']).exists():
+			User.objects.create_user(request.POST['username'], password=request.POST['password'])
+			return redirect('home')
+		else:
+			messages.add_message(request, messages.ERROR, 'Nazwa użytkownika zajęta')
+	elif 'logIn' in request.POST:
+		user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+		if user is not None:
+			login(request, user)
+			return redirect('home')
+		else:
+			messages.add_message(request, messages.ERROR, 'Niepoprawne dane logowania')
+	return redirect('/')
+
+
+def logout(request):
+	if not request.user.is_authenticated:
+		return redirect('/')
+	django.contrib.auth.logout(request)
+	return redirect('/')
